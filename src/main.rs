a15a75e533d2dc6fb1cf1c3823a7219bf580d6f5
simon0302010 <simon0302010@gmail.com>
2025-12-08 19:49:39 +0100

    storing of measurements

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::{collections::BTreeSet, fmt};
+use std::{collections::HashSet, fmt};
+use std::time::Duration;
 
 use arboard::Clipboard;
 use eframe::{NativeOptions, egui};
 use egui::widgets::Label;
+use egui_notify::Toasts;
 
 fn main() -> eframe::Result<()> {
     eframe::run_native(
@@ -12,13 +14,25 @@ fn main() -> eframe::Result<()> {
     )
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, PartialEq, Clone)]
 struct Point {
     x: f32,
     y: f32,
     z: f32,
     yaw: f32,
     pitch: f32,
+}
+
+impl Eq for Point {}
+
+impl std::hash::Hash for Point {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.x.to_bits().hash(state);
+        self.y.to_bits().hash(state);
+        self.z.to_bits().hash(state);
+        self.yaw.to_bits().hash(state);
+        self.pitch.to_bits().hash(state);
+    }
 }
 
 impl fmt::Display for Point {
@@ -34,7 +48,8 @@ impl fmt::Display for Point {
 struct FindEnd {
     clipboard: Clipboard,
     clipboard_text: String,
-    points: BTreeSet<Point>,
+    points: HashSet<Point>,
+    toasts: Toasts
 }
 
 impl Default for FindEnd {
@@ -42,7 +57,8 @@ impl Default for FindEnd {
         Self {
             clipboard: Clipboard::new().expect("failed to initialize clipboard"),
             clipboard_text: String::new(),
-            points: BTreeSet::new(),
+            points: HashSet::new(),
+            toasts: Toasts::default()
         }
     }
 }
@@ -51,11 +67,11 @@ impl eframe::App for FindEnd {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered(|ui| {
-                if ui.button("Get clipboard").clicked() {
+                if ui.button("New measurement").clicked() {
                     let clipboard = self.clipboard.get_text().unwrap_or_default();
                     let filtered: Vec<f32> = clipboard
                         .chars()
-                        .filter(|c| c.is_numeric() || [' ', '.'].contains(c))
+                        .filter(|c| c.is_numeric() || [' ', '.', '-'].contains(c))
                         .collect::<String>()
                         .trim()
                         .split(' ')
@@ -64,14 +80,43 @@ impl eframe::App for FindEnd {
                         .collect();
 
                     if filtered.len() == 5 && clipboard.trim().starts_with('/') {
-                        self.clipboard_text = format!("{:?}", filtered);
+                        let point = Point {
+                            x: filtered.get(0).copied().unwrap_or_default(),
+                            y: filtered.get(1).copied().unwrap_or_default(),
+                            z: filtered.get(2).copied().unwrap_or_default(),
+                            yaw: {
+                                let raw = filtered.get(3).copied().unwrap_or_default();
+                                let normalized = raw % 360.0;
+                                if normalized > 180.0 {
+                                    normalized - 360.0
+                                } else if normalized < -180.0 {
+                                    normalized + 360.0
+                                } else {
+                                    normalized
+                                }
+                            },
+                            pitch: filtered.get(4).copied().unwrap_or_default(),
+                        };
+
+                        self.points.insert(point);
+
+                        self.clipboard_text = format!("{:?}", self.points);
+
+                        self.toasts.info("Stored new point").duration(Duration::from_secs(2));
                     } else {
                         self.clipboard_text = "Failed to parse command.".to_string();
                     }
+                }
+                if ui.button("Clear measurements").clicked() {
+                    self.points.clear();
+                    self.clipboard_text = String::new();
+                    self.toasts.info("Cleared measurements").duration(Duration::from_secs(2));
                 }
                 ui.add_space(10.0);
                 ui.add(Label::new(format!("{}", self.clipboard_text)));
             })
         });
+
+        self.toasts.show(ctx);
     }
 }
